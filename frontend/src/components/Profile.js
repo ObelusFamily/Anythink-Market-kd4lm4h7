1cdca23438ebb7238e8d9a89af072de9641213f5
coffeendude <109362460+coffeendude@users.noreply.github.com>
2022-11-23 20:53:34 +0000

    ran prettier

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -10,7 +10,6 @@ import {
   PROFILE_PAGE_UNLOADED,
 } from "../constants/actionTypes";
 
-
 const EditProfileSettings = (props) => {
   if (props.isUser) {
     return (
@@ -133,7 +132,7 @@ class Profile extends React.Component {
                 src={profile.image}
                 className="user-img"
                 alt={profile.username}
-              />  
+              />
               <h4 className="py-2">{profile.username}</h4>
               <p>{profile.bio}</p>
 
